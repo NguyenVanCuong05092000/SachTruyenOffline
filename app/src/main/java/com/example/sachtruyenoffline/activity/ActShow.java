60acd8eae3ae5be4b9edc6d0b7a4b4eac1255229
NguyenVanCuong05092000 <cuongnguyen0509200@gmail.com>
2019-11-22 11:37:16 +0700

    kkkk

--- app/src/main/java/com/example/sachtruyenoffline/activity/ActShow.java
+++ app/src/main/java/com/example/sachtruyenoffline/activity/ActShow.java
@@ -1,17 +1,30 @@
 package com.example.sachtruyenoffline.activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sachtruyenoffline.R;
 
+import static com.example.sachtruyenoffline.R.color.WhiteColor;
+import static com.example.sachtruyenoffline.R.color.BlackColor;
+import static com.example.sachtruyenoffline.R.color.VangColor;
+import static com.example.sachtruyenoffline.R.color.XamColor;
+
 public class ActShow extends AppCompatActivity {
 LinearLayout lnShow;
+TextView tvDoc;
 RelativeLayout MenuTop, MenuBotton;
+RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +32,30 @@ RelativeLayout MenuTop, MenuBotton;
         lnShow = findViewById(R.id.lnShow);
         MenuBotton = findViewById(R.id.menuBotton);
         MenuTop = findViewById(R.id.MenuTop);
+        radioGroup = findViewById(R.id.groupRadio);
+        tvDoc = findViewById(R.id.tvDoc);
+
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rdBtnTrang:
+                        lnShow.setBackgroundColor(0xffffffff);
+                        tvDoc.setTextColor(0xff000000);
+                        break;
+                    case R.id.rdBtnVang:
+                        lnShow.setBackgroundColor(0xffffff00);
+                        tvDoc.setTextColor(0xff000000);
+                        break;
+                    case R.id.rdBtnXam:
+                        lnShow.setBackgroundColor(0xff000000);
+                        tvDoc.setTextColor(0xffffffff);
+                        break;
+
+                }
+            }
+        });
 
 
 
